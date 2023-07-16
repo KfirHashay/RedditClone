da4bb1cfa03f2f16e2da191db90cd4365c3dbe9d
Kfir Hashay <kfirhashay@outlook.com>
2023-07-16 20:53:46 +0300

    db - navbar - search - auth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { cn } from '@/lib/utils';
 
 import '@/styles/globals.css';
+import Navbar from '@/components/navbar/Navbar';
+import Providers from '@/components/Providers';
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
@@ -11,12 +15,30 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
+  authModal,
 }: {
   children: React.ReactNode;
+  authModal: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        'bg-white text-slate-900 antialiased light',
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <Providers>
+          <Navbar />
+          {authModal}
+
+          <div className="container max-w-7xl mx-auto h-full pt-12">
+            {children}
+          </div>
+        </Providers>
+        {/* <Toaster /> */}
+      </body>
     </html>
   );
 }
